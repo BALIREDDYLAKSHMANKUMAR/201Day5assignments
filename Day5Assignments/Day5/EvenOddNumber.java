f8c058fac57cc90e0df9b3b218d5136a9ba51ce9
lakshmankumar <laxmansgvp@gmail.com>
2022-09-18 16:24:04 +0530

    add files to branchs

---
+++ Day5Assignments/Day5/EvenOddNumber.java
@@ -0,0 +1,20 @@
+
+package Day5Assignments.Day5;
+
+
+public class EvenOddNumber {
+	public static void main(String[] args) {
+
+       
+
+        System.out.print("Enter a number: ");
+        int num =20;
+
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
+
+
