88d4ab7a2894cb71c7e29092ce67e1925c9e6f6c
lakshmankumar <laxmansgvp@gmail.com>
2022-09-18 15:37:43 +0530

    adding files to git

---
+++ Day5Assignments/Day5/SwapNumber.java
@@ -0,0 +1,23 @@
+package Day5Assignments.Day5;
+
+public class SwapNumber {
+	public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+
+        System.out.println("--Before swap--");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+
+        System.out.println("--After swap--");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
